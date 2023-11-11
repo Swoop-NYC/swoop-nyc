cbadb6f68ea7d115941d74b37a7b2809895211b4
Christopher Bock <josebock02@gmail.com>
2023-11-11 09:38:42 -0500

    Adding updated server files to main.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+app.use(express.json());
+
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE_CONNECTION_KEY)
+mongoose.connection.once('open', () => {
+    console.log('Connected to Database');
+  });
+
+
+app.use('*', (err, req, res, next) => {
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 500,
+        message: { err: 'there is an error and its our fualt' }
+    }
+    const errorObj = Object.assign(defaultErr, err);
+    res.status(errorObj.status).json(errorObj.message);
+})
 
 app.listen(3000, ()=>{
     console.log(`server listening on port ${PORT}`);
