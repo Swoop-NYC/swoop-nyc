0e41d9ce4fc74f575780d6d19c0603a17975566a
Christopher Bock <josebock02@gmail.com>
2023-11-10 20:31:47 -0500

    created models/ server is connected to database/ .env is done

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express();
 const PORT = 3000;
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE_CONNECTION_KEY)
+mongoose.connection.once('open', () => {
+    console.log('Connected to Database');
+  });
+
+app.use(express.json());
+
+app.use('*', (err, req, res, next) => {
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 500,
+        message: { err: 'there is an error and its our fualt' }
+    }
+    const errorObj = Object.assign(defaultErr, err);
+    res.status(errorObj.status).json(errorObj.message);
+})
+
+
 
 app.listen(3000, ()=>{
     console.log(`server listening on port ${PORT}`);
