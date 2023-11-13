71ceeac594092b589f3a047bcc9d88e7db7ee815
Cheri French <clynnfrench@gmail.com>
2023-11-13 14:06:58 -0500

    Linked the signup and login page to the App.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,18 +5,21 @@ import SideNav from './SideNav.jsx';
 import '../styles.css'
 import '../../build/calendar.css'
 import CreatePost from './sidenav/CreatePost.jsx';
-
+import Listings from './containers/Listings.jsx';
+import Login from './sidenav/Login.jsx';
+import Signup from './sidenav/SignUp.jsx';
 
 const App = () => {
   return (
     <Router>
     <div>
     <Header></Header>
-{  /*    <Header/> 
-  <SideNav/> */}
     </div>
       <Routes>
         <Route path='/createpost' element={<CreatePost/>}></Route>
+        <Route path='/listings' element={<Listings/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
       </Routes>
     </Router>
 
--- server/controller/itemController.js
+++ server/controller/itemController.js
@@ -1,8 +1,8 @@
 const Item = require('../models/itemModel.js');
 
-const itemController = {
+const itemController = {}
 
-createItem(req, res, next) {
+itemController.createItem = (req, res, next) => {
     console.log('here')
         // const { title, image, description, location, dropTime } = req.body;
 
@@ -26,8 +26,6 @@ createItem(req, res, next) {
             return next({log: 'there is an error it itemController createItem', status: 404, message: {err: 'you are missing information for your created item'}})
         })
 
-    }
-    
     };
 
 module.exports = itemController;
